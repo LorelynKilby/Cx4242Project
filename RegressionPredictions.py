a6e7625995bdf2cd5852c1b09dcaf40972935474
LorelynKilby <LorelynKilby@gmail.com>
2015-12-02 16:35:07 -0500

    Master - Adding files for Jen

--- RegressionPredictions.py
+++ RegressionPredictions.py
@@ -7,7 +7,7 @@ f = open("cleaneddata.csv",'rt')
 reader = csv.reader(f)
 
 predictions = []
-years = range(1960,2014)
+years = range(1960,2000)
 regression_records = [['State','Source','coef','intercept']]
 
 for row in reader:
@@ -15,7 +15,7 @@ for row in reader:
     regression_record = [row[0],row[1]]
     values = []
 
-    for i in range(2,56):
+    for i in range(2,42):
         values.append(float(row[i]))
 
     d = { 'year' : years ,
@@ -29,7 +29,7 @@ for row in reader:
 
     regression_records.append(regression_record)
 
-    for yr in range(2014,2021):
+    for yr in range(2000,2010):
         yv = regression[0]*yr + regression[1]
         newrow.append(yv)
 
@@ -38,10 +38,10 @@ for row in reader:
 
 pprint(predictions)
 
-with open("predictions-Energy.csv",'wb') as f:
+with open("predictions-Energy-2000-2010.csv",'wb') as f:
     writer = csv.writer(f)
     writer.writerows(predictions)
 
-with open("regressionRecords-Energy.csv",'wb') as of:
+with open("regressionRecords-Energy-2000-2010.csv",'wb') as of:
     writer = csv.writer(of)
     writer.writerows(regression_records)
--- convert.py
+++ convert.py
@@ -2,23 +2,13 @@ import csv
 import json
 from pprint import * 
 
-f = open("cleaneddata.csv",'rt')
-reader = csv.reader(f)
-
-predictions = [['State','Soruce','2014','2015','2016','2017','2018','2019','2020']]
-years = range(1960,2014)
-print years
-
-for row in reader:
-    newrow = [row[0],row[1] ]
-
+csvfile = open('policies.csv')
 
 l = []
 i = 1
 for row in csvfile:
     r= row.split(",")
-    print r 
-    newrow = {'id': i, 'content': r[1] , 'start': r[0]}
+    newrow = {"id": i, "type": point 'content': r[1] , 'start': r[2]}
     l.append(newrow)
     i += 1
 
